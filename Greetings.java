7755f03cfcef76e1c8d61f4246dd07a6ef8815f2
pengbomuzzy <xavipengbo@gmail.com>
2014-08-07 14:19:13 -0400

    Create Greetings.java

---
+++ Greetings.java
@@ -0,0 +1,8 @@
+public class Greetings{
+		public static void main(String[] args) {
+			String name = args[0];
+			String place = args[1];
+			System.out.println("Hello " + name + " from " + place);
+			System.out.println("I'VE ALWAYS WANTED TO GO TO " + place.toUpperCase());
+	}
+}
