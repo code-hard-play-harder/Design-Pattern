e48a6012b3fcb303f72a9cc8f8e086a79bda655e
pengbomuzzy <xavipengbo@gmail.com>
2014-08-07 14:20:23 -0400

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,14 @@
+public class Numbers{
+	public static void main(String args[]){
+		int num0 = Integer.parseInt(args[0]);
+		int num1 = Integer.parseInt(args[1]);
+	
+		System.out.println(num0 + num1);
+		System.out.println(num0 - num1);
+		System.out.println(num0 * num1);
+		System.out.println(num0 / num1);
+		System.out.println(num0 % num1);
+	
+	}
+
+}
