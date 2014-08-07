4c387a75dff81c0e4986eaa5096e7a328a2c44be
pengbomuzzy <xavipengbo@gmail.com>
2014-08-07 14:18:39 -0400

    Delete Greetings.java

--- Greetings.java
+++
@@ -1,8 +0,0 @@
-public class Greetings{
-		public static void main(String[] args) {
-		String name = args[0];
-		String place = args[1];
-		System.out.println("Hello " + name + " from " + place);
-		System.out.println("I'VE ALWAYS WANTED TO GO TO " + place.toUpperCase());
-	}
-}
